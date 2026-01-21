4f1d4244c3d7f5c4265aeb752b3f4f5050f869c7
aristrisnawan <aristrisnawan35@gmail.com>
2026-01-21 14:25:02 +0700

    Add page add job

---
+++ app/add-job.tsx
@@ -0,0 +1,134 @@
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { Picker } from "@react-native-picker/picker";
+import { useState } from "react";
+import {
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function AddJobScreen() {
+  const [show, setShow] = useState(false);
+  const [date, setDate] = useState(new Date());
+
+  const onChangeCalendar = () => {
+    const currentDate = new Date() || date;
+    setDate(currentDate);
+  };
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <SafeAreaView style={{ marginHorizontal: 16 }}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingVertical: 16 }}
+        >
+          <View>
+            <Text>Company Name</Text>
+            <TouchableWithoutFeedback>
+              <TextInput
+                placeholder="Input company name"
+                style={{ borderWidth: 1, borderRadius: 10, marginTop: 10 }}
+              />
+            </TouchableWithoutFeedback>
+            <Text style={{ marginTop: 10 }}>Position</Text>
+            <TouchableWithoutFeedback>
+              <TextInput
+                placeholder="Input position"
+                style={{ borderWidth: 1, borderRadius: 10, marginTop: 10 }}
+              />
+            </TouchableWithoutFeedback>
+            <Text style={{ marginTop: 10 }}>Status</Text>
+            <View
+              style={{
+                borderWidth: 1,
+                borderColor: "#000",
+                borderRadius: 10,
+                marginTop: 10,
+                overflow: "hidden",
+              }}
+            >
+              <Picker style={{ height: 55, width: "100%" }}>
+                <Picker.Item label="Applied" value="applied" />
+                <Picker.Item label="Reject" value="reject" />
+              </Picker>
+            </View>
+            <Text style={{ marginTop: 10 }}>Source</Text>
+            <TouchableWithoutFeedback>
+              <TextInput
+                placeholder="Input source"
+                style={{ borderWidth: 1, borderRadius: 10, marginTop: 10 }}
+              />
+            </TouchableWithoutFeedback>
+            <Text style={{ marginTop: 10 }}>Apply Date</Text>
+            <View
+              style={{
+                flexDirection: "row",
+                borderWidth: 1,
+                borderRadius: 10,
+                padding: 8,
+                justifyContent: "space-between",
+                alignItems: "center",
+                marginTop: 10,
+              }}
+            >
+              <Text>
+                {date.toLocaleDateString("id-ID", {
+                  day: "numeric",
+                  month: "long",
+                  year: "numeric",
+                })}
+              </Text>
+              <TouchableOpacity onPress={() => setShow(true)}>
+                <MaterialIcons name="calendar-month" size={24} />
+              </TouchableOpacity>
+            </View>
+            {show && (
+              <DateTimePicker
+                value={date}
+                mode="date"
+                display="calendar"
+                onChange={onChangeCalendar}
+              />
+            )}
+            <Text style={{ marginTop: 10 }}>Notes</Text>
+            <TouchableWithoutFeedback>
+              <TextInput
+                style={{
+                  borderWidth: 1,
+                  borderRadius: 10,
+                  marginTop: 10,
+                  minHeight: 100,
+                  textAlignVertical: "top",
+                }}
+                multiline={true}
+              />
+            </TouchableWithoutFeedback>
+          </View>
+          <View style={{ marginTop: 16 }}>
+            <TouchableOpacity
+              style={{
+                backgroundColor: "#0E21A0",
+                paddingVertical: 10,
+                borderRadius: 10,
+                alignItems: "center",
+              }}
+            >
+              <Text style={{ color: "white", textAlign: "center" }}>
+                Save Job
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
+  );
+}
