05e2c0559c687fadac8af00970148060ae842ae1
aristrisnawan <aristrisnawan35@gmail.com>
2026-01-21 13:13:18 +0700

    Finishing home screen

--- app/home.tsx
+++ app/home.tsx
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   Text,
   TextInput,
+  TouchableOpacity,
   TouchableWithoutFeedback,
   View,
 } from "react-native";
@@ -46,27 +47,77 @@ export default function LandingScren() {
       </View>
       <View style={{ marginTop: 16 }}>
         <Text style={{ fontSize: 20, fontWeight: "600" }}>Job Overview</Text>
-        <ScrollView horizontal contentContainerStyle={{ gap: 10 }}>
+        <ScrollView
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          contentContainerStyle={{ gap: 10 }}
+        >
           {[1, 2, 3, 4, 5].map((_, index) => {
             return (
-              <View
-                key={index}
-                style={{
-                  marginTop: 10,
-                  borderWidth: 1,
-                  borderRadius: 10,
-                  width: 100,
-                  minHeight: 40,
-                  alignItems: "center",
-                  padding: 8,
-                }}
-              >
-                <Text style={{ fontWeight: "600" }}>Applied</Text>
+              <View key={index} style={styles.containerOverview}>
+                <Text style={styles.textOverview}>Applied</Text>
                 <Text>(10)</Text>
               </View>
             );
           })}
         </ScrollView>
+      </View>
+      <View style={{ marginTop: 16 }}>
+        <View style={styles.recentContainer}>
+          <Text style={styles.textRecent}>Recent Jobs</Text>
+          <TouchableOpacity onPress={() => alert("Oke")}>
+            <Text style={styles.textViewAll}>View All</Text>
+          </TouchableOpacity>
+        </View>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ gap: 20 }}
+        >
+          {[1, 2, 3].map((_, index) => {
+            return (
+              <View style={styles.cardRecentJobs} key={index}>
+                <View>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Text style={{ fontWeight: "500" }}>Google</Text>
+                    <View style={styles.containerTitle}>
+                      <Text style={{ color: "white" }}>Applied</Text>
+                    </View>
+                  </View>
+                  <Text style={{ fontWeight: "600" }}>Data Analyst</Text>
+                </View>
+                <Text style={{ fontSize: 12, textAlign: "center" }}>
+                  Applied on 09 Jan: LinkedIn
+                </Text>
+              </View>
+            );
+          })}
+        </ScrollView>
+        <View style={{ marginTop: 16 }}>
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#0E21A0",
+              paddingVertical: 10,
+              borderRadius: 10,
+              alignItems: "center",
+            }}
+          >
+            <Text style={{ color: "white", textAlign: "center" }}>Add Job</Text>
+          </TouchableOpacity>
+          <View style={{ alignItems: "center", marginTop: 50 }}>
+            <Text style={styles.textQuote}>Keep Going</Text>
+            <Text style={styles.textQuote}>
+              You've applied to 10 jobs this month.
+            </Text>
+            <Text style={styles.textQuote}>Stay Consistent!</Text>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -97,5 +148,47 @@ const styles = StyleSheet.create({
     height: 40,
     borderRadius: 50,
     resizeMode: "contain",
+  },
+  containerOverview: {
+    marginTop: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    width: 100,
+    minHeight: 40,
+    alignItems: "center",
+    padding: 8,
+  },
+  textOverview: {
+    fontWeight: "600",
+  },
+  recentContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textRecent: {
+    fontSize: 20,
+    fontWeight: "600",
+  },
+  textViewAll: {
+    fontWeight: "500",
+    color: "#0E21A0",
+  },
+  cardRecentJobs: {
+    width: 180,
+    height: 150,
+    borderWidth: 1,
+    marginTop: 10,
+    padding: 10,
+    justifyContent: "space-between",
+  },
+  containerTitle: {
+    backgroundColor: "#0E21A0",
+    paddingHorizontal: 10,
+    paddingVertical: 4,
+    borderRadius: 20,
+  },
+  textQuote: {
+    fontSize: 18,
+    fontWeight: "500",
   },
 });
